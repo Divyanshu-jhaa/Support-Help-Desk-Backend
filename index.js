98328f8bd7765b66b09672c956829e3090df72a0
divyanshu <divjha123@gmail.com>
2023-07-17 20:49:53 +0530

    fixed an issue

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT=process.env.PORT || 5000;
 const server=require('http').createServer(app);
 const io=new Server(server,{
     cors:{
-        origin:`http://localhost:3000`,
+        origin:"*",
        
     },
 });
